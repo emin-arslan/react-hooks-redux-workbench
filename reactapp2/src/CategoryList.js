3ead41541d6f3106be03b19869d18dd5fac7f087
emin-arslan <eminarslan1864@gmail.com>
2023-03-15 23:04:24 +0300

    Onclick Event

--- reactapp2/src/CategoryList.js
+++ reactapp2/src/CategoryList.js
@@ -2,14 +2,18 @@ import React, { Component } from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      categories: [
-        { categoryId: 1, categoryName: " Baverages" },
-        { categoryId: 2, categoryName: " Condiments" },
-      ],
-    };
+  // constructor(props) {
+  //   super(props);
+  //   this.
+  state = {
+    categories: [
+      { categoryId: 1, categoryName: " Baverages" },
+      { categoryId: 2, categoryName: " Condiments" },
+    ],
+    currentCategory: "",
+  };
+  changeCategory = (category) =>{
+    this.setState({ currentCategory: category.categoryName })
   }
   render() {
     return (
@@ -17,9 +21,17 @@ export default class CategoryList extends Component {
         <h3>{this.props.info.title}</h3>
         <ListGroup>
           {this.state.categories.map((category) => (
-            <ListGroupItem>qdwdqwdqwdwww</ListGroupItem>
+            <ListGroupItem
+              onClick={() =>this.changeCategory(category)
+                
+              }
+              key={category.categoryId}
+            >
+              {category.categoryName}
+            </ListGroupItem>
           ))}
         </ListGroup>
+        <h4>{this.state.currentCategory}</h4>
       </div>
     );
   }
